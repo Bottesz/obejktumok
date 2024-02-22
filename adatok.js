eba84081089b26a2e4cdaf999c64f357021bc41e
Bottesz <botyaring@gmail.com>
2024-02-22 09:35:07 +0100

    mainjs

---
+++ adatok.js
@@ -0,0 +1,26 @@
+const kutyalista=[
+    {
+        nev: "Dézi",
+        fajta:"pitbull",
+        szin:"fekete",
+        marmagassag: 34,
+        szul_datum:2020,
+        szul_hely:"Budapest"
+    }
+    
+    
+    
+    
+    {
+        nev: "Fruzsina",
+        fajta:" keverék",
+        szin:"tarka",
+        marmagassag: 50,
+        szul_datum:2021,
+        szul_hely:"Budapest"
+    }
+
+]
+
+/* hogy érem el Dézi színét */
+console.log(kutyalista[0].szin)
---
+++ main.js
@@ -0,0 +1,7 @@
+let lista = ["első","második"]
+
+console.log(lista[0])
+
+lista[0]="átírt első"
+
+console.log(lista)
